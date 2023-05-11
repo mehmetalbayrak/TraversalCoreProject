43684fc02fd1b5902d3b6095a7a9273ab6cff24e
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-05-11 21:44:31 +0300

    added guide section, admin themes & member settings

---
+++ Business/Concrete/GuideManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class GuideManager : IGuideService
+    {
+        private IGuideDal _guideDal;
+
+        public GuideManager(IGuideDal guideDal)
+        {
+            _guideDal = guideDal;
+        }
+
+        public List<Guide> GetAll()
+        {
+            return _guideDal.GetAll();
+        }
+
+        public Guide GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Guide entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Guide entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Guide entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _GuideList : ViewComponent
+    {
+        GuideManager _guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _guideManager.GetAll();
+            return View(values);
+        }
+    }
+}

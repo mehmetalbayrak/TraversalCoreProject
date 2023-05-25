46f558b605aff584e0ce4f3d236390fe87e5ac3e
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-05-25 22:58:28 +0300

    added mail operations

---
+++ TraversalCoreProject/Models/MailRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCoreProject.Models
+{
+    public class MailRequestModel
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}

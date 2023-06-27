200c3ff58518181b3e433b4d55efebea99a4e07f
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-06-27 15:12:39 +0300

    added exchange and movie apis from rapidapi

---
+++ TraversalCoreProject/Areas/Admin/Controllers/MovieApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    public class MovieApiController : Controller
+    {
+        [Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+            List<MovieApiViewModel> movieapis = new List<MovieApiViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies1.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8f12fb9e51mshd11557ba6519623p1b0f16jsnc4ef2df0bbcf" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                movieapis = JsonConvert.DeserializeObject<List<MovieApiViewModel>>(body);
+                return View(movieapis);
+            }
+        }
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Models/MovieApiViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class MovieApiViewModel
+    {
+        [JsonProperty("rank")]
+        public int Rank { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+
+        [JsonProperty("year")]
+        public int Year { get; set; }
+
+        [JsonProperty("trailer")]
+        public string Trailer { get; set; }
+    }
+}

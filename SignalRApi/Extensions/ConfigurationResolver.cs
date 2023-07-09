bf674f16f7ff6372a02987ee7df14de8b6c08b4c
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-07-09 22:16:06 +0300

    added postgre sql connection for signalR

---
+++ SignalRApi/Extensions/ConfigurationResolver.cs
@@ -0,0 +1,16 @@
+﻿namespace SignalRApi.Extensions
+{
+    public  class ConfigurationResolver
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetDbConnectionString()
+        {
+            return _configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
---
+++ SignalRApi/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using SignalRApi.DataAccess;
+using SignalRApi.Extensions;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt=>
+{
+    opt.UseNpgsql("Server=localhost;Port=5432;Database=VisitorDb;User Id=postgres;Password=12345");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

3343cffc5c7983b042c3bd1b8742e6be1cad73a5
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-07-10 17:44:32 +0300

    added consume project

--- SignalRApi/Models/VisitorService.cs
+++ SignalRApi/Models/VisitorService.cs
@@ -30,7 +30,7 @@ namespace SignalRApi.Models
             List<VisitorChart> visitorCharts = new List<VisitorChart>();
             using (var command = _context.Database.GetDbConnection().CreateCommand())
             {
-                command.CommandText = "";
+                command.CommandText = "select * from crosstab('Select VisitDate,City,VisitCount From Visitors Order By 1,2') AS ct (VisitDate Date,City1 int, City2 int, City3 int,City4 int,City5 int)";
                 command.CommandType = System.Data.CommandType.Text;
                 _context.Database.OpenConnection();
                 using (var reader = command.ExecuteReader())
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -3,6 +3,7 @@ using SignalRApi.DataAccess;
 using SignalRApi.Extensions;
 using Microsoft.Extensions.Configuration;
 using SignalRApi.Models;
+using SignalRApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +13,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt=>
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
 {
     opt.UseNpgsql("Server=localhost;Port=5432;Database=TraversalVisitorDb;User Id=postgres;Password=12345");
 });
 builder.Services.AddScoped<VisitorService>();
 builder.Services.AddSignalR();
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    }));
 
 var app = builder.Build();
 
@@ -29,6 +39,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<VisitorHub>("/VisitorHub");
+});
 
 app.MapControllers();
 
